6244a569f4c5bc3d8673f4c45af2b79da33d148a
Karan Agrawal <karan220690@gmail.com>
2022-05-31 18:51:46 +0530

    first commit

---
+++ src/main/java/in/ashok/AshokRm6H2DbAppApplication.java
@@ -0,0 +1,13 @@
+package in.ashok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshokRm6H2DbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshokRm6H2DbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ashok/AshokRm6H2DbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshokRm6H2DbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
